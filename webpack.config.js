7fd209f8f54f6174b09e535c55eb2a2fe87f1bab
A.D. Slaton <ad.slaton@turner.com>
2016-10-14 15:57:22 -0400

    add chunk hashing

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     entry: {
-        'injector.standalone': './src/entries/injector.standalone.js'
+        'injector': './src/entries/injector.standalone.js'
     },
     output: {
         path: __dirname,
-        filename: 'bundles/[name].js'
+        filename: 'bundles/[name].[chunkhash:8].js'
     }
 };
