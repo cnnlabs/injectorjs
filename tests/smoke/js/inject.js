e59405134b42b010fc6eb435e909f0641edcae42
A.D. Slaton <ad.slaton@turner.com>
2016-11-01 15:12:27 -0400

    0.11.0

--- tests/smoke/js/inject.js
+++ tests/smoke/js/inject.js
@@ -1,4 +1,4 @@
-var WebpackAssets = [{"name":"bundles/injector.b97de41c.js","size":90667,"chunks":[0],"chunkNames":["injector"],"emitted":true}],
+var WebpackAssets = [{"name":"bundles/injector.5bb7b337.js","size":90729,"chunks":[0],"chunkNames":["injector"],"emitted":true}],
     src = '../../../' + WebpackAssets[0].name;
 
 function loadScript(url, callback)
