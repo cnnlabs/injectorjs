ac396c2bc1e57ea738b93238b90a9c673585e81b
marty_trujillo <marty@ilikeemail.com>
2016-11-05 23:13:46 -0400

    Adds slimerjs code to test for expected text in smoke tests.

--- tests/smoke/slimer/tests.js
+++ tests/smoke/slimer/tests.js
@@ -5,8 +5,18 @@ webpage
     // store a screenshot of the page
     webpage.viewportSize =
         { width:650, height:320 };
-    webpage.render('pics/smoke001.png',
-                   {onlyViewport:true});
+    webpage.render('pics/smoke001.png', {onlyViewport:true});
+
+    var mainText = webpage.evaluate(function () {
+          return document.querySelector("#injector-success").textContent;
+    });
+
+    slimer.wait(2000);
+
+    if (mainText === 'Resource 01 Was Loaded And Executed Successfully') {
+      console.log('Expected Text Found In smoke001.');
+    }
+
     // then open a second page
     return webpage.open('http://localhost:8080/tests/smoke/smoke002-listens-for-complete-event.html');
   })
@@ -14,16 +24,32 @@ webpage
     // store a screenshot of the page
     webpage.viewportSize =
         { width:650, height:320 };
-    webpage.render('pics/smoke002.png',
-                   {onlyViewport:true});
+    webpage.render('pics/smoke002.png', {onlyViewport:true});
 
-      // then open a third page
-      return webpage.open('http://localhost:8080/tests/smoke/smoke003-loads-standalone.html');
+    var mainText = webpage.evaluate(function () {
+         return document.querySelector("#injector-success").textContent;
+    });
+
+    if (mainText === "Injector Ready Captured And Processed") {
+      console.log('Expected Text Found In smoke002.');
+    }
+    slimer.wait(2000);
+    // then open a third page
+    return webpage.open('http://localhost:8080/tests/smoke/smoke003-loads-standalone.html');
+
     })
     .then(function(){
        // store a screenshot of the page
       webpage.viewportSize ={ width:650, height:320 };
       webpage.render('pics/smoke003.png',{onlyViewport:true});
 
-        slimer.exit()
+      var mainText = webpage.evaluate(function () {
+           return document.querySelector("#injector-success").textContent;
+      });
+
+      if (mainText === "Injector Ready Captured And Processed") {
+        console.log('Expected Text Found In smoke003.');
+      }
+      slimer.wait(2000);
+      slimer.exit()
     });
